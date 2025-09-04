c6d39315f9ecdac353f9d2f8171b84f80748b17a
Hanaa011 <hanaasaloufi@gmail.com>
2025-09-04 14:01:03 +0300

    init commit

--- src/test/java/assignments/Day02/Task1.java
+++ src/test/java/assignments/Day02/Task1.java
@@ -1,4 +1,5 @@
 package assignments.Day02;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,21 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class Task1 {
 
-
     WebDriver driver;
-    //Task 1: Browser History Exploration
-//Setup:
+
+    // Setup
     @BeforeEach
     void setUp() {
-
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         System.out.println("Test is running");
-
     }
 
     @AfterEach
@@ -29,32 +25,37 @@ public class Task1 {
         System.out.println("Test finished");
     }
 
-
-
+    // Test 1: Navigation back and forward
     @Test
     public void test1() {
-
         driver.navigate().to("https://www.wikipedia.org");
         System.out.println(driver.getTitle());
         Assertions.assertTrue(driver.getTitle().contains("Wikipedia"));
+
         driver.navigate().to("https://www.google.com");
         Assertions.assertTrue(driver.getTitle().contains("Google"));
 
-
         driver.navigate().back();
         Assertions.assertTrue(driver.getTitle().contains("Wikipedia"));
+
         driver.navigate().forward();
         Assertions.assertTrue(driver.getTitle().contains("Google"));
 
         driver.navigate().back();
         Assertions.assertTrue(driver.getTitle().contains("Wikipedia"));
+
         driver.navigate().forward();
         Assertions.assertTrue(driver.getTitle().contains("Google"));
+    }
 
+    // Test 2: Refresh on Google
+    @Test
+    public void test2() {
+        driver.navigate().to("https://www.google.com");
+        Assertions.assertTrue(driver.getTitle().contains("Google"));
+        System.out.println("Page title contains Google");
 
-
-
-
+        driver.navigate().refresh();
+        Assertions.assertTrue(driver.getTitle().contains("Google"));
     }
 }
-
